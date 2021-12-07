4fc2ab8fb27bdfc9314a1a86b5fd60cae063e186
pipipear <65990558+pipipear@users.noreply.github.com>
2021-12-07 10:14:57 -0800

    Update Script.ps1
    dark mode and wallpaper

--- Script.ps1
+++ Script.ps1
@@ -1,2 +1,13 @@
+# Remove startup appliations
 Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name *
 Remove-Item ([Environment]::GetFolderPath('Startup') + '\*.*')
+
+# Enable dark mode
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+
+# Bypass wallpaper restrictions
+Remove-Item ($env:APPDATA + '\Microsoft\Windows\Themes\*') 
+Invoke-WebRequest -Uri "https://i.imgur.com/RzQqHgo.jpeg" -OutFile ($env:APPDATA + '\Microsoft\Windows\Themes\TranscodedWallpaper') 
+
+# Restart desktop shell
+Stop-Process -ProcessName explorer
