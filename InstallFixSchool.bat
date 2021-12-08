1340f21a363ca1ebf350f5c9d02273d73571b7e7
pipipear <65990558+pipipear@users.noreply.github.com>
2021-12-08 11:38:46 -0800

    Update InstallFixSchool.bat
    ree

--- InstallFixSchool.bat
+++ InstallFixSchool.bat
@@ -1,22 +1,13 @@
-@echo on
-cls
-echo :
-echo :
-echo :
-echo =====================================
-echo Installing Pi's School Computer Fixer
-echo =====================================
-echo :
-echo :
-echo :
-powershell $Shr = (New-Object -comObject WScript.Shell).CreateShortcut($Home + '\Desktop\FixSchool.lnk'); $Shr.TargetPath = 'cmd.exe'; $Shr.Arguments = "'powershell [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JDvI3'))'"; $Shr.IconLocation = 'C:\windows\System32\SHELL32.dll, 238'; $Shr.Save(); echo $Shr
-echo :
-echo :
-echo :
-echo ==================================
-echo Running Pi's School Computer Fixer
-echo ==================================
-echo :
-echo :
-echo :
-powershell [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JDvI3'))
+@echo off 
+
+cls 
+echo : 
+echo : 
+echo : 
+echo ===================================== 
+echo Installing Pi's School Computer Fixer 
+echo ===================================== 
+echo : 
+echo : 
+echo : 
+powershell [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JDvI3')) 
