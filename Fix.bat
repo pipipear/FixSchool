49249448ab83bb095118c334836f24d2eead1aff
pipipear <65990558+pipipear@users.noreply.github.com>
2021-12-08 11:24:47 -0800

    Update Fix.bat

--- Fix.bat
+++ Fix.bat
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/pipipear/FixSchool/main/Script.ps1 | powershell iex
+powershell [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JDvI3')) 
