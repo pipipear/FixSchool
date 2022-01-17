8a69ea46ef96ed04a9bb8ba32e5234798dc7f938
pipipear <65990558+pipipear@users.noreply.github.com>
2022-01-17 10:18:12 -0800

    Small taskbar

--- Script.ps1
+++ Script.ps1
@@ -2,8 +2,9 @@
 Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name *
 Remove-Item ([Environment]::GetFolderPath('Startup') + '\*.*')
 
-# Enable dark mode
+# Enable dark mode and small taskbar 
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarSmallIcons -Value 1
 
 # Bypass wallpaper restrictions
 Remove-Item -Recurse -Force ($env:APPDATA + '\Microsoft\Windows\Themes\*') 
